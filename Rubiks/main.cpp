baa863bbff45e693434b18a5beef94a67be8754c
Ellis Sparky Hoag <ellishoag@Elliss-MacBook-Pro.local>
2014-08-04 18:10:40 -0500

    Initial Commit

---
+++ Rubiks/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rubiks
+//
+//  Created by Ellis Sparky Hoag on 8/4/14.
+//  Copyright (c) 2014 Ellis Sparky Hoag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
